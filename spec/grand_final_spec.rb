4ca05c257381cee1f5ea6825a361cb7b628f814b
Chris Ward <chriswarddev@gmail.com>
2015-02-09 07:01:27 +0000

    grand finale all planes can take off again and change their stauts to flying

--- spec/grand_final_spec.rb
+++ spec/grand_final_spec.rb
@@ -12,7 +12,7 @@ describe "The grand finale (last spec)" do
 	let(:gatwick) {Airport.new}
 
 	def land_six_planes
-	gatwick.sunny!
+		gatwick.sunny!
   	gatwick.land_plane(spitfire)
   	gatwick.land_plane(hurricane)
   	gatwick.land_plane(lancaster)
@@ -21,23 +21,37 @@ describe "The grand finale (last spec)" do
   	gatwick.land_plane(swordfish)
 	end
 
+	def take_off_six_planes
+		gatwick.sunny!
+		gatwick.release_plane(spitfire)
+  	gatwick.release_plane(hurricane)
+  	gatwick.release_plane(lancaster)
+  	gatwick.release_plane(mosquito)
+  	gatwick.release_plane(typhoon)
+  	gatwick.release_plane(swordfish)
+	end
+
   
   it 'all planes can land' do
   	land_six_planes
   	expect(gatwick).to be_full
   end
 
-	it 'all planes change there status to landed' do
+	it 'all planes change their status to landed' do
 		land_six_planes
 		expect(spitfire).to_not be_flying
 		expect(swordfish).to_not be_flying
-
 	end
 
 	it 'all planes can take off again' do
+		take_off_six_planes
+  	expect(gatwick).to_not be_full
 	end
 
-	it 'all planes change there status to flying' do
+	it 'all planes change their status to flying' do
+		take_off_six_planes
+		expect(spitfire).to be_flying
+		expect(swordfish).to be_flying
 	end
 
 end
